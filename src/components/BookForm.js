b07fe5c09f9710389e1f9e1f8ac7caf45802e070
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-12-15 15:44:57 +0100

    Configure Redux store

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
+// import useDispatch hook
+import { useDispatch } from 'react-redux';
+// import your Action Creators
+import { addBook, removeBook } from '../redux/books/books';
+
+const dispatch = useDispatch();
+
+const submitBookToStore = () => {
+  const newBook = {
+    id, // make sure it's unique
+    title,
+    author,
+  };
+
+  // dispatch an action and pass it the newBook object (your action's payload)
+  dispatch(addBook(newBook));
+};
+
+  <button onClick={submitBookToStore}>Add Book</button>;
+
 const BookForm = () => (
   <form action="">
-    <input placeholder="Author" />
+    <input placeholder="Title" />
     <input placeholder="Author" />
     <button type="submit">Add book</button>
   </form>
