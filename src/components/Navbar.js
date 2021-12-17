1d0f712b816c96adbfff871dd1726fd8960eeb59
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-12-17 16:29:47 +0100

    Change Navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import './nav.css';
 
 const Navbar = () => (
   <nav className="navbar">
-    <h1 className="nav1">BookStore</h1>
+    <h1 className="nav1">BOOKSTORE CMS</h1>
     <ul className="navList">
       <li><Link to="/books" className="nav2">Books</Link></li>
       <li><Link to="/categories" className="nav3">Categories</Link></li>
